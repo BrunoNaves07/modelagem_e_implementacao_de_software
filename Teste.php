245f111809785c89ce97c74f7b6dac0722d16816
Bruno <bruno.naves@estudante.ufla.br>
2021-09-19 12:56:08 -0300

    Criaçao da funçao soma na classe Teste

--- Teste.php
+++ Teste.php
@@ -7,20 +7,33 @@
      private $valX;
      private $valY;
      
+     /**
+      * Construtor
+      */
      public function __construct($valX, $valY) {
          $this->valX = $valX;
          $this->valY = $valY;
      }
 
+     /**
+      * Get valor de X
+      */
      public function getValX() {
          return $this->valX;
      }
 
+     /**
+      * Get valor de Y
+      */
      public function getValY() {
          return $this->valY;
      }
 
+     /**
+      * Soma
+      */
      public function soma() {
-         return $this->valX + $this->valY;
+         return $this->getValX() + $this->getValY();
      }
+
  }
